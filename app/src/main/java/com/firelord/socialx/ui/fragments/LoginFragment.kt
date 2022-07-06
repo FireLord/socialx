b96fe4b3c26fd7a9d11032739b7976cb3b239443
FireLord <firelord.xda@gmail.com>
2022-07-06 12:56:54 +0530

    add google sign in option
    Signed-off-by: FireLord <firelord.xda@gmail.com>

--- app/src/main/java/com/firelord/socialx/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/firelord/socialx/ui/fragments/LoginFragment.kt
@@ -1,8 +1,10 @@
 package com.firelord.socialx.ui.fragments
 
 import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,7 +15,16 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.firelord.socialx.R
 import com.firelord.socialx.databinding.FragmentLoginBinding
+import com.firelord.socialx.util.Constants.Companion.RC_SIGN_IN
+import com.firelord.socialx.util.Constants.Companion.TAG
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginFragment : Fragment() {
 
@@ -26,6 +37,9 @@ class LoginFragment : Fragment() {
     private lateinit var firebaseAuth : FirebaseAuth
     private var email = ""
     private var password = ""
+
+    // Googel Auth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,8 +54,22 @@ class LoginFragment : Fragment() {
         progressDialog.setMessage("Logging in..")
         progressDialog.setCanceledOnTouchOutside(false)
 
+        // call google
+        val googleSignInOption = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(),googleSignInOption)
+
         // init firebaseauth
         firebaseAuth = FirebaseAuth.getInstance()
+
+        // Google Sign in button
+        binding.btGoogle.setOnClickListener {
+            Log.d(TAG,"onCreate: begin google SignIn")
+            val intent = googleSignInClient.signInIntent
+            startActivityForResult(intent, RC_SIGN_IN)
+        }
 
         // handle click, open sign up page
         binding.tvNoAccount.setOnClickListener {
@@ -56,6 +84,61 @@ class LoginFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // Request returned from launching the intent
+        if (requestCode == RC_SIGN_IN){
+            Log.d(TAG,"onActivityResult: Google sign in intent")
+            val accountTask = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign in success
+                val account = accountTask.getResult(ApiException::class.java)
+                firebaseAuthWithGoogleAccount(account)
+            }
+            catch (e:Exception){
+                // failed google signin
+                Log.d(TAG,"onActivityResult: ${e.message}")
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogleAccount(account: GoogleSignInAccount?) {
+        Log.d(TAG,"firebaseAuthWithGoogleAccount: begin firebase auth with google")
+        val credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnSuccessListener { authResult ->
+                //login success
+                Log.d(TAG,"firebaseAuthWithGoogleAccount: LoggedIn")
+
+                // Get logged in user
+                val firebaseUser = firebaseAuth.currentUser
+
+                // get user id
+                val uid = firebaseUser!!.uid
+                val email = firebaseUser!!.email
+
+                Log.d(TAG,"firebaseAuthWithGoogleAccount: uid: ${uid}")
+                Log.d(TAG,"firebaseAuthWithGoogleAccount: email: ${email}")
+
+                //check if user is new or existing
+                if (authResult.additionalUserInfo!!.isNewUser){
+                    //user is new - account created
+                    Log.d(TAG,"firebaseAuthWithGoogleAccount: Account created ...\n$email")
+                    Toast.makeText(activity,"Account created $email",Toast.LENGTH_SHORT).show()
+                } else {
+                    // existing user - logged in
+                    Log.d(TAG,"firebaseAuthWithGoogleAccount: Existing user \n$email")
+                    Toast.makeText(activity, "Logged in as $email",Toast.LENGTH_SHORT).show()
+                }
+                // open newsHome
+                view?.findNavController()?.navigate(R.id.action_tabsFragment_to_newsHomeFragment)
+            }
+            .addOnFailureListener { e ->
+                Log.d(TAG,"firebaseAuthWithGoogleAccount: Login failed due to ${e.message}")
+                Toast.makeText(activity, "Login failed due to ${e.message}",Toast.LENGTH_SHORT).show()
+            }
     }
 
     private fun validateData() {
--- app/src/main/java/com/firelord/socialx/util/Constants.kt
+++ app/src/main/java/com/firelord/socialx/util/Constants.kt
@@ -6,5 +6,7 @@ class Constants {
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
+        const val RC_SIGN_IN = 100
+        const val TAG = "GOOGLE_SIGN_IN_TAG"
     }
 }
